d946c7a82582481067a393901db3e8c0829ff4c7
legewae <legendaryway0@gmail.com>
2025-06-25 11:23:36 +0600

    task01_Palyndrome

---
+++ practice2025/task01/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+            input = input.ToLower();
+            string newStr = "";
+            for(int i = 0; i<input.Length; i++)
+            {
+                if (char.IsPunctuation(input[i]) || char.IsWhiteSpace(input[i])) continue;
+
+                newStr += input[i];
+            }
+            if (newStr.Length == 0) return false;
+
+            return newStr.ToCharArray().SequenceEqual(newStr.Reverse());
+        }
+    }
+}
